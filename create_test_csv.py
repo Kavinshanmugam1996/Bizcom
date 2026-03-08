cb2c081a9aa6832961a18e7173e9f3a4c09774dc
shanm <shanm@example.com>
2026-03-07 19:02:12 -0500

    chore: remove unuseful debug and temporary test files

--- create_test_csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.DataFrame({
-    "Question": ["What is ML?", "Is ML dangerous?"],
-    "Answer": ["Machine Learning", "It depends"]
-})
-
-df.to_csv("test.csv", index=False)
-print("test.csv created")
